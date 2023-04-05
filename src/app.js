61e0a01ae5fe91d2df08511f6bfdcddbbb91e877
robmab <rob_mb@outlook.es>
2023-04-05 20:41:13 +0200

    Font card

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 /* SELECTORES */
 let icons = document.querySelectorAll("#icon");
 let number = document.querySelector("#number");
+let button = document.querySelector(".btn");
+
+let inputWidth = document.querySelector("#inputWidth");
+let inputHeight = document.querySelector("#inputHeight");
 
 /* FUNCIÓN */
 const cardContentPaloValor = () => {
@@ -19,12 +23,48 @@ const cardContentPaloValor = () => {
     });
   } else {
     icons.forEach(p => {
+      p.style.color = "black"
       p.innerHTML = paloPinta
     })
   }
+
+  clearTimeout(timer); //reset timer
+  timer = window.setInterval(cardContentPaloValor, 10000);
 }
 
 /* EVENTOS */
-window.onload = () => cardContentPaloValor();
+window.onload = () => cardContentPaloValor(); //On-load
 
-console.log(document.querySelector("#number"))
+let timer = window.setInterval(cardContentPaloValor, 10000); //Timer
+
+button.addEventListener("click", cardContentPaloValor) //Randomizer
+
+
+//Change card size
+inputWidth.addEventListener("change", (e) => {
+  let value = e.target.value;
+  let min = e.target.min;
+  let max = e.target.max;
+  console.log(e)
+
+  if (value != "" && min <= value && max >= value) {
+    e.target.placeholder = value;
+    e.target.defaultValue = value
+    document.querySelector(".card").style.width = value + "px";
+  }
+})
+
+inputHeight.addEventListener("change", (e) => {
+  let value = e.target.value;
+  let min = e.target.min;
+  let max = e.target.max;
+
+  if (value != "" && min <= value && max >= value) {
+    e.target.placeholder = value;
+    e.target.setAttribute("value", value)
+    document.querySelector(".card").style.height = value + "px"
+  }
+})
+
+
+
